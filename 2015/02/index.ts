1155ecf76ec07b4561318e21b04462d74422e44d
Fabrice Lamant <flamant@shutterstock.com>
2024-11-21 11:53:05 +0000

    2015/02

---
+++ 2015/02/index.ts
@@ -0,0 +1,24 @@
+import AoCPuzzle from '../../puzzle';
+
+export default class Puzzle extends AoCPuzzle {
+  public async part1(): Promise<string | number> {
+    let totalSize = 0;
+    for (const line of this.lines) {
+      const [x, y, z] = line.split(/x/gi).map(Number).sort((a, b) => a - b);
+      totalSize += 2 * x * y
+        + 2 * x * z
+        + 2 * y * z
+        + x * y;
+    }
+    return totalSize;
+  }
+
+  public async part2(): Promise<string | number> {
+    let totalSize = 0;
+    for (const line of this.lines) {
+      const [x, y, z] = line.split(/x/gi).map(Number).sort((a, b) => a - b);
+      totalSize += (x + x + y + y) + (x * y * z);
+    }
+    return totalSize;
+  }
+}
