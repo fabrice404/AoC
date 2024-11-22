cb9d4cd3adc5142b650549e6d945db1ea81041ca
Fabrice Lamant <flamant@shutterstock.com>
2024-11-22 21:47:23 +0000

    2015/06

---
+++ 2015/06/index.ts
@@ -0,0 +1,58 @@
+import AoCPuzzle from '../../puzzle';
+
+export default class Puzzle extends AoCPuzzle {
+  private ngrid: number[][] = [];
+
+  public async part1(): Promise<string | number> {
+    this.ngrid = new Array(1000).fill(0).map(() => new Array(1000).fill(0));
+
+    for (const line of this.lines) {
+      const [, action, x1, y1, x2, y2] = line.match(/(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)/)!;
+      for (let x = +x1; x <= +x2; x += 1) {
+        for (let y = +y1; y <= +y2; y += 1) {
+          switch (action) {
+            case 'turn on':
+              this.ngrid[x][y] = 1;
+              break;
+            case 'turn off':
+              this.ngrid[x][y] = 0;
+              break;
+            case 'toggle':
+              this.ngrid[x][y] = this.ngrid[x][y] === 0 ? 1 : 0;
+              break;
+            default: break;
+          }
+        }
+      }
+    }
+    return this.ngrid.flat().filter((x) => x === 1).length;
+  }
+
+  public async part2(): Promise<string | number> {
+    this.ngrid = new Array(1000).fill(0).map(() => new Array(1000).fill(0));
+
+    for (const line of this.lines) {
+      const [, action, x1, y1, x2, y2] = line.match(/(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)/)!;
+      for (let x = +x1; x <= +x2; x += 1) {
+        for (let y = +y1; y <= +y2; y += 1) {
+          switch (action) {
+            case 'turn on':
+              this.ngrid[x][y] += 1;
+              break;
+            case 'turn off':
+              this.ngrid[x][y] -= 1;
+              if (this.ngrid[x][y] < 0) {
+                this.ngrid[x][y] = 0;
+              }
+              break;
+            case 'toggle':
+              this.ngrid[x][y] += 2;
+              break;
+            default: break;
+          }
+        }
+      }
+    }
+    return this.ngrid.flat().reduce((acc, val) => acc + val, 0);
+  }
+}
--- 2015/README.md
+++ 2015/README.md
@@ -7,3 +7,4 @@ Day | Part 1 | Part 1 | Part 2 | Part 2
 3 | 0.132ms <br/><sub><sup>4</sup></sub> | 1.434ms <br/><sub><sup>2081</sup></sub> | 0.045ms <br/><sub><sup>3</sup></sub> | 1.381ms <br/><sub><sup>2341</sup></sub> 
 4 | 480.998ms <br/><sub><sup>609043</sup></sub> | 92.733ms <br/><sub><sup>117946</sup></sub> | 5919.102ms <br/><sub><sup>6742839</sup></sub> | 3302.719ms <br/><sub><sup>3938038</sup></sub> 
 5 | 0.108ms <br/><sub><sup>2</sup></sub> | 4.583ms <br/><sub><sup>238</sup></sub> | 0.138ms <br/><sub><sup>2</sup></sub> | 0.989ms <br/><sub><sup>69</sup></sub> 
+6 | 41.164ms <br/><sub><sup>998996</sup></sub> | 104.171ms <br/><sub><sup>377891</sup></sub> | 26.604ms <br/><sub><sup>2000001</sup></sub> | 106.430ms <br/><sub><sup>14110788</sup></sub> 
