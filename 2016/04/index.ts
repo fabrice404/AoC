9822b3ebc2e1a42ef54592c68406f053ea4ce310
Fabrice Lamant <flamant@shutterstock.com>
2024-11-24 21:31:05 +0000

    2016/04

---
+++ 2016/04/index.ts
@@ -0,0 +1,66 @@
+import AoCPuzzle from '../../puzzle';
+
+export default class Puzzle extends AoCPuzzle {
+  public async part1(): Promise<string | number> {
+    const validSectors: number[] = [];
+    for (const line of this.lines) {
+      const [name, sector, checksum] = line.match(/([a-z-]+)-(\d+)\[([a-z]+)\]/)!.slice(1);
+      const charCounts: { [char: string]: number } = {};
+      for (const char of name.replace(/-/g, '')) {
+        charCounts[char] = (charCounts[char] || 0) + 1;
+      }
+
+      const sortedChars = Object.keys(charCounts).sort((a, b) => {
+        if (charCounts[a] === charCounts[b]) {
+          return a < b ? -1 : 1;
+        }
+        return charCounts[b] - charCounts[a];
+      });
+
+      if (sortedChars.slice(0, 5).join('') === checksum) {
+        validSectors.push(+sector);
+      }
+    }
+    return validSectors.reduce((acc, val) => acc + val, 0);
+  }
+
+  public async part2(): Promise<string | number> {
+    const validRooms = [];
+    for (const line of this.lines) {
+      const [name, sector, checksum] = line.match(/([a-z-]+)-(\d+)\[([a-z]+)\]/)!.slice(1);
+      const charCounts: { [char: string]: number } = {};
+      for (const char of name.replace(/-/g, '')) {
+        charCounts[char] = (charCounts[char] || 0) + 1;
+      }
+
+      const sortedChars = Object.keys(charCounts).sort((a, b) => {
+        if (charCounts[a] === charCounts[b]) {
+          return a < b ? -1 : 1;
+        }
+        return charCounts[b] - charCounts[a];
+      });
+
+      if (sortedChars.slice(0, 5).join('') === checksum) {
+        validRooms.push({ name, sector });
+      }
+    }
+    const alphabet = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz';
+
+    return validRooms
+      .map((room) => {
+        const { name, sector } = room;
+        const decryptedName = name.split('').map((char) => {
+          if (char === '-') {
+            return ' ';
+          }
+          return alphabet[alphabet.indexOf(char) + (+sector % 26)];
+        }).join('');
+        return { name: decryptedName, sector };
+      })
+      .filter((room) => room.name.includes('very') || room.name.includes('north'))
+      .map((room) => `${room.name}, ${room.sector}`)
+      .sort()
+      .join('\n')
+      .trim();
+  }
+}
--- 2016/README.md
+++ 2016/README.md
@@ -5,3 +5,4 @@ Day | Part 1 | Part 1 | Part 2 | Part 2
 1 | 0.030ms <br/><sub><sup>12</sup></sub> | 0.111ms <br/><sub><sup>231</sup></sub> | 0.090ms <br/><sub><sup>4</sup></sub> | 0.293ms <br/><sub><sup>147</sup></sub> 
 2 | 0.071ms <br/><sub><sup>1985</sup></sub> | 0.122ms <br/><sub><sup>78985</sup></sub> | 0.059ms <br/><sub><sup>5DB3</sup></sub> | 0.144ms <br/><sub><sup>57DD8</sup></sub> 
 3 | 0.038ms <br/><sub><sup>1</sup></sub> | 2.277ms <br/><sub><sup>1032</sup></sub> | 0.076ms <br/><sub><sup>0</sup></sub> | 1.137ms <br/><sub><sup>1838</sup></sub> 
+4 | 0.192ms <br/><sub><sup>1514</sup></sub> | 9.367ms <br/><sub><sup>361724</sup></sub> | 0.158ms <br/><sub><sup>very encrypted name, 343</sup></sub> | 6.605ms <br/><sub><sup>northpole object storage, 482</sup></sub> 
