00867ab06df9c9e635d808dc558525c660997ee8
Fabrice Lamant <flamant@shutterstock.com>
2024-11-26 10:03:43 +0000

    2017/06

---
+++ 2017/06/index.ts
@@ -0,0 +1,55 @@
+import AoCPuzzle from '../../puzzle';
+
+export default class Puzzle extends AoCPuzzle {
+  private blocksHistory: string[] = [];
+
+  private blocks: number[] = [];
+
+  private addHistory(blocks: number[]): boolean {
+    const key = blocks.join(',');
+    if (this.isExample) {
+      console.log(key);
+    }
+    if (!this.blocksHistory.includes(key)) {
+      this.blocksHistory.push(key);
+      return true;
+    }
+    return false;
+  }
+
+  private findMaxIndex(blocks: number[]): number {
+    for (let i = 0; i < blocks.length; i += 1) {
+      if (blocks[i] === Math.max(...blocks)) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  public async part1(): Promise<string | number> {
+    this.blocks = this.input.split(/\s/gi).filter(Boolean).map(Number);
+    this.blocksHistory = [];
+    let iterations = 0;
+
+    while (this.addHistory(this.blocks)) {
+      const index = this.findMaxIndex(this.blocks);
+      const toAdd = this.blocks[index];
+      this.blocks[index] = 0;
+
+      let currentIndex = index + 1;
+      for (let i = 0; i < toAdd; i += 1) {
+        if (currentIndex >= this.blocks.length) {
+          currentIndex = 0;
+        }
+        this.blocks[currentIndex] += 1;
+        currentIndex += 1;
+      }
+      iterations += 1;
+    }
+    return iterations;
+  }
+
+  public async part2(): Promise<string | number> {
+    return this.blocksHistory.length - this.blocksHistory.indexOf(this.blocks.join(','));
+  }
+}
--- 2017/README.md
+++ 2017/README.md
@@ -7,3 +7,4 @@ Day | Part 1 | Part 1 | Part 2 | Part 2
 3 | 0.047ms <br/><sub><sup>31</sup></sub> | 1.632ms <br/><sub><sup>438</sup></sub> | 0.086ms <br/><sub><sup>1968</sup></sub> | 1.130ms <br/><sub><sup>266330</sup></sub> 
 4 | 0.043ms <br/><sub><sup>2</sup></sub> | 0.438ms <br/><sub><sup>455</sup></sub> | 0.076ms <br/><sub><sup>3</sup></sub> | 2.142ms <br/><sub><sup>186</sup></sub> 
 5 | 0.436ms <br/><sub><sup>5</sup></sub> | 119.185ms <br/><sub><sup>358131</sup></sub> | 2.434ms <br/><sub><sup>10</sup></sub> | 8206.510ms <br/><sub><sup>25558839</sup></sub> 
+6 | 0.515ms <br/><sub><sup>5</sup></sub> | 141.684ms <br/><sub><sup>12841</sup></sub> | 0.057ms <br/><sub><sup>4</sup></sub> | 0.026ms <br/><sub><sup>8038</sup></sub> 
