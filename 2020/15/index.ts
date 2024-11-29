30562d59fb4753118e7a53969b0cbbe556de5238
Fabrice Lamant <flamant@shutterstock.com>
2024-11-29 19:16:27 +0000

    2020/15

---
+++ 2020/15/index.ts
@@ -0,0 +1,37 @@
+import AoCPuzzle from '../../puzzle';
+
+export default class Puzzle extends AoCPuzzle {
+
+  private run(iterations: number): number {
+    const numbers = new Map<number, number[]>();
+    const inputs = this.input.split(',').map(Number);
+    const startIndex = inputs.length;
+    let lastNumber = -1;
+
+    for (let i = 0; i < inputs.length; i += 1) {
+      const n = inputs[i];
+      numbers.set(n, [...(numbers.get(n) || []), i]);
+      lastNumber = n;
+    }
+
+    for (let i = startIndex; i < iterations; i += 1) {
+      const num = numbers.get(lastNumber)!;
+      if (num.length < 2) {
+        lastNumber = 0;
+      } else {
+        const [last, prev] = num;
+        lastNumber = last - prev;
+      }
+      numbers.set(lastNumber, [i, ...(numbers.get(lastNumber) || []).slice(0, 1)]);
+    }
+    return lastNumber;
+  }
+
+  public async part1(): Promise<string | number> {
+    return this.run(2020);
+  }
+
+  public async part2(): Promise<string | number> {
+    return this.run(30_000_000);
+  }
+}
--- 2020/README.md
+++ 2020/README.md
@@ -6,3 +6,4 @@ Day | Part 1 | Part 1 | Part 2 | Part 2
 12 | 0.059ms <br/><sub><sup>25</sup></sub> | 0.321ms <br/><sub><sup>1565</sup></sub> | 0.042ms <br/><sub><sup>286</sup></sub> | 0.257ms <br/><sub><sup>78883</sup></sub> 
 13 | 0.073ms <br/><sub><sup>295</sup></sub> | 1.360ms <br/><sub><sup>4722</sup></sub> | 0.124ms <br/><sub><sup>1068781</sup></sub> | 0.289ms <br/><sub><sup>825305207525452</sup></sub> 
 14 | 0.181ms <br/><sub><sup>165</sup></sub> | 1.962ms <br/><sub><sup>8566770985168</sup></sub> | 0.179ms <br/><sub><sup>208</sup></sub> | 8229.638ms <br/><sub><sup>4832039794082</sup></sub> 
+15 | 0.514ms <br/><sub><sup>436</sup></sub> | 0.425ms <br/><sub><sup>319</sup></sub> | 8818.716ms <br/><sub><sup>175594</sup></sub> | 8956.351ms <br/><sub><sup>2424</sup></sub> 
