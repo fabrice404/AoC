98936890a003b1d5320fc22d47eaaf49974b64c0
Fabrice Lamant <flamant@shutterstock.com>
2024-11-30 16:40:05 +0000

    2019/04

---
+++ 2019/04/index.ts
@@ -0,0 +1,34 @@
+import { countLetters } from '../../helpers/string';
+import AoCPuzzle from '../../puzzle';
+
+export default class Puzzle extends AoCPuzzle {
+  private validPasswords: string[] = [];
+
+  public async part1(): Promise<string | number> {
+    const [min, max] = this.input.split('-').map(Number);
+    for (let i = min; i <= max; i += 1) {
+      const [a, b, c, d, e, f] = i.toString().split('');
+      if (
+        i.toString().length === 6 && // six-digit number
+        (a === b || b === c || c === d || d === e || e === f) && // two adjacent digits are the same
+        (a <= b && b <= c && c <= d && d <= e && e <= f) // only increase or stay the same
+      ) {
+        this.validPasswords.push(i.toString());
+      }
+    }
+
+    return this.validPasswords.length;
+  }
+
+  public async part2(): Promise<string | number> {
+    let valid = 0;
+
+    for (const password of this.validPasswords) {
+      if ([...countLetters(password).values()].filter((n) => n === 2).length) {
+        valid += 1;
+      }
+    }
+
+    return valid;
+  }
+}
