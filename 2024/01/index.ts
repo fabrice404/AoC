8b4560a229af5db2b6e01de5dd28f0e37ecfb1bd
Fabrice Lamant <flamant@shutterstock.com>
2024-12-01 08:07:11 +0000

    2024/01

---
+++ 2024/01/index.ts
@@ -0,0 +1,31 @@
+import { countItems } from '../../helpers/array';
+import AoCPuzzle from '../../puzzle';
+
+export default class Puzzle extends AoCPuzzle {
+  private listA: number[] = [];
+
+  private listB: number[] = [];
+
+  public async part1(): Promise<string | number> {
+    for (const line of this.lines) {
+      const [a, b] = line.split(/\s/gi).filter(Boolean).map(Number);
+      this.listA.push(a);
+      this.listB.push(b);
+    }
+
+    this.listA.sort((a, b) => a - b);
+    this.listB.sort((a, b) => a - b);
+
+    return this.listA
+      .map((n, i) => Math.abs(this.listB[i] - n))
+      .reduce((acc, val) => acc + val, 0);
+  }
+
+  public async part2(): Promise<string | number> {
+    const itemsB = countItems(this.listB);
+
+    return this.listA
+      .map((n) => n * (itemsB.get(n) || 0))
+      .reduce((acc, val) => acc + val, 0);
+  }
+}
---
+++ 2024/README.md
@@ -0,0 +1,5 @@
+# 2024
+
+Day | Part 1 | Part 1 | Part 2 | Part 2
+:---:|---:|---:|---:|---:
+1 | 0.070ms <br/><sub><sup>11</sup></sub> | 0.712ms <br/><sub><sup>2367773</sup></sub> | 0.037ms <br/><sub><sup>31</sup></sub> | 0.129ms <br/><sub><sup>21271939</sup></sub> 
